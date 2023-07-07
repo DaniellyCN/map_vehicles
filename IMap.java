f696f03af6af001123601318374b0e05431c4916
DaniellyCN <89779359+DaniellyCN@users.noreply.github.com>
2023-07-06 23:45:55 -0300

    classe vetor em andamento

--- IMap.java
+++ IMap.java
@@ -5,12 +5,9 @@ public interface IMap {
     public boolean containsValue(Veiculo veiculo) throws Exception;
     public Veiculo getVeiculo(int chassi) throws Exception;
     public Veiculo putVeiculo(int chassi, Veiculo veiculo) throws Exception;
-    public Veiculo remove(int chassi) throws Exception;
+    public boolean remove(int chassi) throws Exception;
     public void putAllVeiculos() throws Exception; // conferir qual deve ser o parâmetro, se tiver um
     public void clear();
-    //public Set<Integer> keySet() throws Exception;   COMO FAZ ESSA
-    //values
-    //entrySet
     public boolean equals() throws Exception; // conferir qual deve ser o parâmetro
     public int codeHash() throws Exception;
 }
